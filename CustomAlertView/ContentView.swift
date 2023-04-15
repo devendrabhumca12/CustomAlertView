10e011f94b9fe1eb3674622eb6d492b1c4b649ad
Devendra Kumar <devendra.kumar@nagarro.com>
2023-04-16 00:13:18 +0530

    Initial Commit

---
+++ CustomAlertView/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CustomAlertView
+//
+//  Created by Devendra Agnihotri on 16/04/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CustomAlertView/CustomAlertViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomAlertViewApp.swift
+//  CustomAlertView
+//
+//  Created by Devendra Agnihotri on 16/04/23.
+//
+
+import SwiftUI
+
+@main
+struct CustomAlertViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
